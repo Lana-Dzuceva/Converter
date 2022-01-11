f14665aeea14fe5fe67b1475537f6f1a07b4e554
lanadzuceva@gmail.com <lanadzuceva@gmail.com>
2022-01-11 09:45:25 +0300

    Добавьте файлы проекта.

---
+++ Converter/FormMain.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Converter
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.comboBoxInitiallyMeasure = new System.Windows.Forms.ComboBox();
+            this.comboBoxFinalMeasure = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(63, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите длину";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(413, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ". Перевести в ";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(195, 155);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(85, 20);
+            this.textBoxInput.TabIndex = 2;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(417, 237);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOutput.TabIndex = 3;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCalculate.Location = new System.Drawing.Point(245, 231);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(131, 29);
+            this.buttonCalculate.TabIndex = 4;
+            this.buttonCalculate.Text = "Перевести";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // comboBoxInitiallyMeasure
+            // 
+            this.comboBoxInitiallyMeasure.FormattingEnabled = true;
+            this.comboBoxInitiallyMeasure.Location = new System.Drawing.Point(286, 155);
+            this.comboBoxInitiallyMeasure.Name = "comboBoxInitiallyMeasure";
+            this.comboBoxInitiallyMeasure.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxInitiallyMeasure.TabIndex = 5;
+            // 
+            // comboBoxFinalMeasure
+            // 
+            this.comboBoxFinalMeasure.FormattingEnabled = true;
+            this.comboBoxFinalMeasure.Location = new System.Drawing.Point(536, 157);
+            this.comboBoxFinalMeasure.Name = "comboBoxFinalMeasure";
+            this.comboBoxFinalMeasure.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxFinalMeasure.TabIndex = 6;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxFinalMeasure);
+            this.Controls.Add(this.comboBoxInitiallyMeasure);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.Text = "Конвертер длин";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.ComboBox comboBoxInitiallyMeasure;
+        private System.Windows.Forms.ComboBox comboBoxFinalMeasure;
+    }
+}
+
---
+++ Converter/FormMain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Converter
+{
+    public partial class FormMain : Form
+    {
+        Dictionary<string, double> measures = new Dictionary<string, double>()
+{
+            {"Millimeter",  1000},
+            {"Centimeter", 100},
+            {"Meter", 1},
+            {"Kilometer", 0.001 }
+};
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            foreach (var measure in measures)
+            {
+                comboBoxInitiallyMeasure.Items.Add(measure.Key);
+                comboBoxFinalMeasure.Items.Add(measure.Key);
+            }
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            var num = textBoxInput.Text;
+            if (num.Length == 0)
+            {
+                MessageBox.Show("Строка ввода пуста");
+            }
+            else if (comboBoxInitiallyMeasure.Text.Length == 0)
+            {
+                MessageBox.Show("Введите размерность числа");
+            }
+            else if (comboBoxFinalMeasure.Text.Length == 0)
+            {
+                MessageBox.Show("Введите размерность перевода");
+            }
+            else
+            {
+                textBoxOutput.Text = Convert.ToString(int.Parse(num) * measures[comboBoxFinalMeasure.Text] / measures[comboBoxInitiallyMeasure.Text]);
+            }
+
+        }
+    }
+}
